d2245369b7faa7a5d0b3c4fa08793ae213a3923b
Bob Kraft <bkraft@phdata.com>
2021-02-09 15:38:04 -0500

    Added parametrize

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 from main import factors_of
+import pytest
 
 
 def test_factors_of_1():
@@ -12,3 +13,19 @@ def test_factors_of_1():
     # Then return list factors
     expected_value = []
     assert result == expected_value
+
+
+@pytest.mark.parametrize("test_input, expected_result",
+                         [(1, []),
+                          ])
+def test_factors_of_integers(test_input, expected_result):
+
+    # Given: integer
+    value = 1
+
+    # When: factoring integer
+    result = factors_of(value)
+
+    # Then return list factors
+    expected_result = []
+    assert result == expected_result
